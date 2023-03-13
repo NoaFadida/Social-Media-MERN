52103e3a7244ad4679b2cae38019211c3d0837a0
NoaFadida <noafadida122@gmail.com>
2023-03-13 11:04:23 +0200

    change login style

--- front/components/LoginPage.tsx
+++ front/components/LoginPage.tsx
@@ -9,14 +9,57 @@ import {
   TextInput,
   Alert,
   ScrollView,
-} from "react-native"
+  Image,
+} from "react-native";
 import AuthModel, { User } from "../model/AuthModel";
-import AsyncStorage from "@react-native-async-storage/async-storage"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AntDesign } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+//web: 616359255354-9548lr7mhkfdnl9hjdj0mgmk7csb2g3b.apps.googleusercontent.com
+//IOS: 616359255354-p775225005q9eppnheomo9h3nb5v2gh1.apps.googleusercontent.com
+// Android:
+
+WebBrowser.maybeCompleteAuthSession();
 
 const LoginPage: FC<{ navigation: any }> = ({ navigation }) => {
   const [password, onText2Change] = useState<string>("");
   const [username, onText1Change] = useState<string>("");
+  const [accessToken, setAccessToken] = useState<string>("");
+  const [user, setUser] = useState<any>(null);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId:
+      "616359255354-9548lr7mhkfdnl9hjdj0mgmk7csb2g3b.apps.googleusercontent.com",
+    iosClientId:
+      "616359255354-p775225005q9eppnheomo9h3nb5v2gh1.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setAccessToken(response.authentication?.accessToken || "");
+      accessToken && fetchUserInfo();
+    }
+  }, [response, accessToken]);
+
+  async function fetchUserInfo() {
+    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    const useInfo = await response.json();
+    setUser(useInfo);
+  }
+
+  const ShowUserInfo = () => {
+    if (user) {
+      return (
+        <View>
+          <Text>hello</Text>
+          <Text>{user.name}</Text>
+        </View>
+      );
+    }
+  };
   //Stay Log In
   useEffect(() => {
     AsyncStorage.getItem("refreshToken").then(async (token) => {
@@ -76,20 +119,34 @@ const LoginPage: FC<{ navigation: any }> = ({ navigation }) => {
         />
 
         <View style={styles.buttonsContainer}>
-          <TouchableOpacity  style={styles.buttonText} onPress={pressHandlerLogin}>
+          <TouchableOpacity
+            style={styles.buttonText}
+            onPress={pressHandlerLogin}
+          >
             <AntDesign name="login" size={30} color="#161a1d" />
             <Text>LogIn</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={styles.buttonText} onPress={pressHandlerSignUp}>
+          <TouchableOpacity
+            style={styles.buttonText}
+            onPress={pressHandlerSignUp}
+          >
             <FontAwesome name="user-plus" size={30} color="#161a1d" />
             <Text>SignUp</Text>
           </TouchableOpacity>
         </View>
         <View>
           <Text style={styles.text}>or</Text>
+          <TouchableOpacity
+            disabled={!request}
+            onPress={() => {
+              promptAsync();
+            }}
+          >
+            <Text>SignIn with google</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
@@ -101,7 +158,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   content: {
-    marginVertical:150
+    marginVertical: 150,
   },
   input: {
     height: 45,
@@ -111,7 +168,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 20,
     borderColor: "#d3d3d3",
-    backgroundColor:"#f8f9fa"
+    backgroundColor: "#f8f9fa",
   },
   buttonsContainer: {
     flexDirection: "row",
@@ -128,7 +185,7 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     padding: 10,
-    color:"white"
+    color: "white",
   },
   text: {
     alignSelf: "center",
